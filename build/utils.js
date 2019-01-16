71301e24ac215cb0ad6336899cd5050eb5e32077
wangxuquan <wangxuquan@meituan.com>
2019-01-16 19:52:27 +0800

    修改11

--- build/utils.js
+++ build/utils.js
@@ -7,9 +7,9 @@ const packageConfig = require('../package.json')
 exports.assetsPath = function (_path) {
   const assetsSubDirectory = process.env.NODE_ENV === 'production'
     ? config.build.assetsSubDirectory
-    : config.dev.assetsSubDirectory
+    : config.dev.assetsSubDirectory;
 
-  return path.posix.join(assetsSubDirectory, _path)
+  return path.posix.join(assetsSubDirectory, _path);
 }
 
 exports.cssLoaders = function (options) {
