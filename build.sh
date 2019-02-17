eee2c3e8a84f0bdde14786debdb3a647479d90c2
WangSuwen <1002853070@qq.com>
2019-02-17 09:27:49 +0800

    构建

--- build.sh
+++ build.sh
@@ -2,14 +2,16 @@
 DIST="dist"
 NODE_MODULES="node_modules"
 echo "git pulling ..."
-git pull
+git pull origin master
 if [ -d $DIST ]; then
+  echo -e "\033[35m 删除 $DIST 目录... \033[0m"
   rm -rf $DIST
 fi
 if [ -d $NODE_MODULES ]; then
+  echo -e "\033[35m 删除 $NODE_MODULES 目录... \033[0m"
   rm -rf $NODE_MODULES
 fi
-echo "npm installing..."
+echo -e "\033[35m npm installing... \033[0m"
 npm i
-echo "webpack building..."
+echo -e "\033[35m webpack building... \033[0m"
 npm run build:prod
